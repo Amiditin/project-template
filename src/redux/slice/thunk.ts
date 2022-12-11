7352dea633f0e85911db4ae82ec9efa1b357a50a
Dmitry Fomin <dimafomin02@gmail.com>
2022-12-11 21:01:20 +0300

    Fix redux thunk

--- src/redux/slice/thunk.ts
+++ src/redux/slice/thunk.ts
@@ -1,10 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
 
-import { apiRoutes } from '@/services/apiRoutes';
 import { getDataRequest } from '@/services/dataService';
 
-export const getData = createAsyncThunk<any[]>(apiRoutes.DATA, async () => {
+export const getData = createAsyncThunk<any[]>('slice/getData', async () => {
   try {
     const { data } = await getDataRequest();
 
