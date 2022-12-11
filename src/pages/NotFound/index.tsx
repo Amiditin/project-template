ad2b9abbe616101b14f97ce24754c4f6f2ebacb3
Dmitry Fomin <dimafomin02@gmail.com>
2022-12-11 15:55:33 +0300

    Add NotFound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import styles from './NotFound.module.scss';
+import { Link } from 'react-router-dom';
+import { Button, Result } from 'antd';
 
 interface NotFoundProps {}
 
 export const NotFound: React.FC<NotFoundProps> = () => {
-  return <div className={styles.notFound}>Page not found</div>;
+  return (
+    <div className={styles.notFound}>
+      <Result
+        className={styles.box}
+        status="404"
+        title="404"
+        subTitle="Страница не найдена. Попробуйте найти то, что вам нужно, с главной страницы."
+        extra={
+          <Link to="/">
+            <Button type="primary">На главную</Button>
+          </Link>
+        }
+      />
+    </div>
+  );
 };
