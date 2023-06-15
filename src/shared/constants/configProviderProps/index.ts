6e8afddc73ce413129c21a18e55395d77eda7382
Dmitry Fomin <dimafomin02@gmail.com>
2023-06-15 03:03:01 +0300

    feat: :sparkles: added fonts for antd theme token

--- src/shared/constants/configProviderProps/index.ts
+++ src/shared/constants/configProviderProps/index.ts
@@ -2,4 +2,12 @@ import RU from 'antd/locale/ru_RU';
 
 import type { ConfigProviderProps } from 'antd/es/config-provider';
 
-export const configProviderProps: ConfigProviderProps = { locale: RU };
+export const configProviderProps: ConfigProviderProps = {
+  locale: RU,
+  theme: {
+    token: {
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+  },
+};
