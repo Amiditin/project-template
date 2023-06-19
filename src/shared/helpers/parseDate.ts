b16b32db7f2c403bbb28700a35c878e589fee244
Dmitry Fomin <dimafomin02@gmail.com>
2023-06-20 01:47:24 +0300

    refactor: :recycle: changed realization index.ts

--- src/shared/helpers/parseDate.ts
+++ src/shared/helpers/parseDate.ts
@@ -1,5 +1,3 @@
 import dayjs from 'dayjs';
 
 export const parseDate = (date: string) => (date ? dayjs(date).format('DD.MM.YYYY HH:mm') : null);
-
-export const calculateAge = (dateBirth: string) => dayjs().diff(dayjs(dateBirth), 'year');
